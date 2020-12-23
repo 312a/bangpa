16548cd7088630b972dbd917d8a798f26be20ede
312a <cnwlsguq1@naver.com>
2020-12-23 18:53:15 +0900

    Initial commit

---
+++ src/main/java/com/jh/bangpa/BangpaApplication.java
@@ -0,0 +1,13 @@
+package com.jh.bangpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangpaApplication.class, args);
+	}
+
+}
